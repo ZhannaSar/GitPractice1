f37c7a4b6c7ffa1f128ffdb62eef916ddd83d76d
ZhannaSar <zsarsen000@citymail.cuny.edu>
2021-06-04 21:46:28 -1000

    Add more work after publishing to github

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Adding Github now");
+        System.out.println("YAY STUFFS ARE ON GITHUB");
     }
+
 }
